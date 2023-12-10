c6fa3f9d54e4a63eb019479936dd44afb34e7a72
NEYROSICK <dhatak24@gmail.com>
2023-12-10 15:30:32 +0200

    Update AdvertItem.js
    1. Updated Advert Item, added all needed fields.
    2. Fixed semantics

--- src/components/catalog/advertItem/AdvertItem.js
+++ src/components/catalog/advertItem/AdvertItem.js
@@ -2,10 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const AdvertItem = ({ advert }) => {
+  const {
+    img,
+    make: brand,
+    model,
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    type,
+    id,
+    accessories,
+    functionalities,
+  } = advert;
+
   return (
     <li>
-      <img src={advert.img} alt={advert.make} width={200} />
-      <p>{advert.make}</p>
+      <img src={img} alt={brand} width={200} />
+      <div>
+        <h3>
+          {brand}
+          <span> {model}</span>, {year}
+        </h3>
+        <span>{rentalPrice}</span>
+      </div>
+      <ul>
+        <li>{address.split(", ")[1]}</li>
+        <li>{address.split(", ")[2]}</li>
+        <li>{rentalCompany}</li>
+        <li>Premium {type}</li>
+        <li>{brand}</li>
+        <li>{id}</li>
+        <li>{accessories[2]}</li>
+      </ul>
+      <button>Learn more</button>
     </li>
   );
 };
